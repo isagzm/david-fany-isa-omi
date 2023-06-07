f903328f6cee208228736b93911afa30e3280e25
Fany Martinez <estefania.marhen@gmail.com>
2023-06-07 16:40:03 +0100

    add first excercise

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function moneyFormat(amount) {
+  return "$" + amount;
+}
+console.log(moneyFormat(19.99));
+
+// const value1 = moneyFormat(19.99)
+// console.log(value1) // $19.99
+
+// const value2 = moneyFormat(250)
+// console.log(value2) // $250.00
+
+// const value3 = moneyFormat(9500.95)
+// console.log(value3) // $9,500.95
