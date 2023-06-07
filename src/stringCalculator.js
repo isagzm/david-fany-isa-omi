0f2edb8ff289aad26dc3aa04aab00262d9f4cd7b
Isabel González <isaabelglez@gmail.com>
2023-06-07 17:51:23 +0100

    add stringCalculator one

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,8 @@
 export function stringCalculator(numbers) {
-  return 15;
+  const split = numbers.split(",");
+
+  const firstNumber = Number(split[0]);
+  const secondNumber = Number(split[1]);
+
+  return firstNumber + secondNumber;
 }
