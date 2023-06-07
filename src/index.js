43d1863f92d76d87972f12ba66451c52f25e4da5
Isabel González <isaabelglez@gmail.com>
2023-06-07 16:52:33 +0100

    add second exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+const value1 = moneyFormat(19.99);
+console.log(value1); // $19.99
+
+const value2 = moneyFormat(250);
+console.log(value2); // $250.00
+
+const value3 = moneyFormat(9500.95);
+console.log(value3); // $9,500.95
+
+/**
+ *
+ * @param {number} amount
+ * @returns
+ */
 function moneyFormat(amount) {
-  return "$" + amount;
+  return "$" + amount.toFixed(2);
 }
-console.log(moneyFormat(19.99));
-
-// const value1 = moneyFormat(19.99)
-// console.log(value1) // $19.99
-
-// const value2 = moneyFormat(250)
-// console.log(value2) // $250.00
-
-// const value3 = moneyFormat(9500.95)
-// console.log(value3) // $9,500.95
